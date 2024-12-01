0f3a7b5bc823a90d75ee0c0365bf7403e72965a5
BoRamSon <onbrson1@gmail.com>
2024-12-01 17:54:53 +0900

    chore: 필요없는 라인 제거

--- src/constants/inputMessages.js
+++ src/constants/inputMessages.js
@@ -1,12 +1,6 @@
 const INPUT_MESSAGES = {
   inputCoachNames: '코치의 이름을 입력해 주세요. (, 로 구분)',
   inputDontEatMenu: '(이)가 못 먹는 메뉴를 입력해 주세요.',
-  input3: '',
-  input4: '',
-  input5: '',
-  input6: '',
-  input7: '',
-  input8: '',
 };
 
 export default INPUT_MESSAGES;
