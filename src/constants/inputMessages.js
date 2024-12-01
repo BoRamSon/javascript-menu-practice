6caa59d85a5c6485f0bdc4c298a90c3dc8d39ff5
BoRamSon <onbrson1@gmail.com>
2024-12-01 16:15:06 +0900

    feat(input): add constant inputMessages and add InputView (readCoachNames, readDontEatMenu)

---
+++ src/constants/inputMessages.js
@@ -0,0 +1,12 @@
+const INPUT_MESSAGES = {
+  inputCoachNames: '코치의 이름을 입력해 주세요. (, 로 구분)',
+  inputDontEatMenu: '(이)가 못 먹는 메뉴를 입력해 주세요.',
+  input3: '',
+  input4: '',
+  input5: '',
+  input6: '',
+  input7: '',
+  input8: '',
+};
+
+export default INPUT_MESSAGES;
