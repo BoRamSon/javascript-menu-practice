c079a4e29507c1811739fd72d61efb54a2d76048
BoRamSon <onbrson1@gmail.com>
2024-12-01 17:51:49 +0900

    feat(App.js): [입력] 각 코치가 못 먹는 메뉴를 입력받는다. (, 로 구분)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import InputCoachNames from './module/InputCoachNamesModule.js';
 import PrintGreeting from './module/PrintGreetingModule.js';
+import InputCoachNames from './module/InputCoachNamesModule.js';
+import InputDontEatMenu from './module/InputDontEatMenuModule.js';
 
 const SAMPLE = {
   일식: '규동, 우동, 미소시루, 스시, 가츠동, 오니기리, 하이라이스, 라멘, 오코노미야끼',
@@ -12,11 +13,14 @@ const SAMPLE = {
 class App {
   constructor() {
     this.inputCoachNames = new InputCoachNames();
+    this.inputDontEatMenu = new InputDontEatMenu();
   }
 
   async play() {
+    const sampleMenu = this.sampleMenu;
     new PrintGreeting();
     const coachNamesArray = await this.inputCoachNames.run();
+    const dd = await this.inputDontEatMenu.run(coachNamesArray, sampleMenu);
   }
 }
 
